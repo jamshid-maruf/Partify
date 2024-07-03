0b34aaa7771bba11fb682c37d453856ce1b125ea
axrordev <axror.algo@gmail.com>
2024-07-03 21:26:54 +0500

    Added AdImageController

---
+++ src/Partify.WebApi/Controllers/AdImageController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Partify.Service.Configurations;
+using Partify.WebApi.ApiServices.AdImages;
+using Partify.WebApi.Models.AdImages;
+using Partify.WebApi.Models.Commons;
+
+namespace Partify.WebApi.Controllers;
+
+public class AdImageController(IAdImageApiService adImageApiService) : BaseController
+{
+    [HttpPost("Create")]
+    public async ValueTask<IActionResult> CreateAsync(AdImageCreateModel createModel, IFormFile formFile)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await adImageApiService.CreateAsync(createModel.AdId, formFile)
+        });
+    }
+
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await adImageApiService.DeleteAsync(id)
+        });
+    }
+
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetByIdAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await adImageApiService.GetByIdAsync(id)
+        });
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllAsync(long adId, Filter filter)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await adImageApiService.GetAllAsync(adId, filter)
+        });
+    }
+}
