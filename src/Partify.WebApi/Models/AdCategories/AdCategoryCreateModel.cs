f721858d52db152273977d2c76c9fe2b8135196b
Jismatov895 <Jismatov895@gmail.com>
2024-07-05 08:33:21 +0500

    Added Ad Category Service, ApiService, Controller

---
+++ src/Partify.WebApi/Models/AdCategories/AdCategoryCreateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Partify.WebApi.Models.AdCategories
+{
+    public class AdCategoryCreateModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Partify.WebApi/Models/AdCategories/AdCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Partify.WebApi.Models.AdCategories
+{
+    public class AdCategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
