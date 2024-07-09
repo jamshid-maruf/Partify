312f9284405f938df601294a0ea757b98545ead0
Jismatov895 <Jismatov895@gmail.com>
2024-07-09 21:15:49 +0500

    Added  Ad Facility

---
+++ src/Partify.WebApi/Models/AdFacilities/AdFacilityViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Partify.WebApi.Models.Ads;
+using Partify.WebApi.Models.Facilities;
+
+namespace Partify.WebApi.Models.AdFacilities;
+
+public class AdFacilityViewModel
+{
+    public long Id { get; set; }
+    public AdViewModel Ad { get; set; }
+    public FacilityViewModel Facility { get; set; }
+}
