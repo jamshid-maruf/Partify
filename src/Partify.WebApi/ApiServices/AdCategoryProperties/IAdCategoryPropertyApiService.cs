26b413c8bcc657de72e8598fda87d8fc040ac514
Jismatov895 <Jismatov895@gmail.com>
2024-07-09 21:08:06 +0500

    Added Ad Category Property

---
+++ src/Partify.WebApi/ApiServices/AdCategoryProperties/IAdCategoryPropertyApiService.cs
@@ -0,0 +1,18 @@
+﻿using Partify.Service.Configurations;
+using Partify.WebApi.Models.AdCategoryProperties;
+
+namespace Partify.WebApi.ApiServices.AdCategoryProperties;
+
+public interface IAdCategoryPropertyApiService
+{
+    ValueTask<AdCategoryPropertyViewModel> CreateAsync(AdCategoryPropertyCreateModel createModel);
+    ValueTask<AdCategoryPropertyViewModel> UpdateAsync(long id, AdCategoryPropertyUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<AdCategoryPropertyViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<AdCategoryPropertyViewModel>> GetAllByAdCategoryIdAsync(long categoryId);
+    ValueTask<IEnumerable<AdCategoryPropertyViewModel>> GetAllAsync(
+        PaginationParams @params,
+        Filter filter,
+        string search = null);
+}
+
---
+++ src/Partify.WebApi/Models/AdCategoryProperties/AdCategoryPropertyCreateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Partify.WebApi.Models.AdCategoryProperties;
+
+public class AdCategoryPropertyCreateModel
+{
+    public string Name { get; set; }
+    public long AdCategoryId { get; set; }
+}
