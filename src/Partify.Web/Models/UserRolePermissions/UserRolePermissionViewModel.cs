1cb68f195a4d0db10f0ef83545336a8089595fd0
axrordev <axror.algo@gmail.com>
2024-07-13 11:15:09 +0500

    added UserRolePermission

---
+++ src/Partify.Web/Models/UserRolePermissions/UserRolePermissionViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using Partify.Web.Models.UserRoles;
+
+namespace Partify.Web.Models.UserRolePermissions;
+
+public class UserRolePermissionViewModel
+{
+	public long Id { get; set; }
+	public UserRoleViewModel UserRole { get; set; }
+	//public PermissionViewModel Permission { get; set; }
+}
---
+++ src/Partify.Web/WebServices/UserRolePermissions/IUserRolePermission.cs
@@ -0,0 +1,14 @@
+﻿using Partify.Service.Configurations;
+using Partify.Web.Models.UserRolePermissions;
+
+namespace Partify.Web.WebServices.UserRolePermissions;
+
+public interface IUserRolePermission
+{
+    ValueTask<UserRolePermissionViewModel> CreateAsync(UserRolePermissionCreateModel createModel);
+    ValueTask<UserRolePermissionViewModel> UpdateAsync(long id, UserRolePermissionUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<UserRolePermissionViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<UserRolePermissionViewModel>> GetAlByRoleIdAsync(long roleId);
+    ValueTask<IEnumerable<UserRolePermissionViewModel>> GetAllAsync();
+}
