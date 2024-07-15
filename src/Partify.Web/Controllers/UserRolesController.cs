0cdaf759f873ef9ac2fcf00af3e7baa47420daf4
Jismatov895 <Jismatov895@gmail.com>
2024-07-15 11:47:11 +0500

    Added User Roles Controller

---
+++ src/Partify.Web/Controllers/UserRolesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Partify.Web.Models.UserRoles;
+using Partify.Web.WebServices.UserRoles;
+
+namespace Partify.Web.Controllers;
+
+public class UserRolesController(IUserRoleWebService userRoleWebService) : Controller
+{
+    public async ValueTask<IActionResult> Index()
+    {
+        var userRoles = await userRoleWebService.GetAllAsync();
+        return View(userRoles);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> Create(UserRoleCreateModel createModel)
+    {
+        var result = await userRoleWebService.CreateAsync(createModel);
+        if (result is not null)
+            return RedirectToAction("Index");
+
+        return View();
+    }
+
+    public async ValueTask<IActionResult> Edit(long id)
+    {
+        var result = await userRoleWebService.GetByIdAsync(id);
+        return View(result);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> Edit(long id, UserRoleUpdateModel updateModel)
+    {
+        var result = await userRoleWebService.UpdateAsync(id, updateModel);
+        if (result is not null)
+            return RedirectToAction("Index");
+
+        return View();
+    }
+
+    public async ValueTask<IActionResult> Delete(long id)
+    {
+        var result = await userRoleWebService.DeleteAsync(id);
+        if (result)
+            return RedirectToAction("Index");
+
+        return View();
+    }
+}
