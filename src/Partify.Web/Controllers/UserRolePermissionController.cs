609f40172e621940c61ed78cb91f6e6471e56691
axrordev <axror.algo@gmail.com>
2024-07-15 08:30:14 +0500

    Active class

---
+++ src/Partify.Web/Controllers/UserRolePermissionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Partify.Web.WebServices.UserRolePermissions;
+
+namespace Partify.Web.Controllers
+{
+    public class UserRolePermissionController(IUserRolePermissionWebService userRolePermissionWebService) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
